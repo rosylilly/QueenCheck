78c2324c99ef1d4653e860990f87654567e2910a
Sho Kusano <rosylilly@aduca.org>
2012-01-21 16:36:58 +0900

    文字の範囲がおかしかったのを修正

--- lib/queencheck/string.rb
+++ lib/queencheck/string.rb
@@ -12,23 +12,23 @@ class String
     if seed == 0
       return ''
     elsif seed < 0.1
-      charset = (0x61 ... 0x7a).to_a + (0x41 ... 0x5a).to_a
+      charset = (0x61 ... 0x7b).to_a + (0x41 ... 0x5b).to_a
     elsif seed < 0.3 # ASCII
-      charset = (0x32 ... 0x7e).to_a
+      charset = (0x32 ... 0x7f).to_a
     elsif seed < 0.5
-      charset = (0x00 ... 0x7f).to_a
+      charset = (0x00 ... 0x80).to_a
     elsif seed < 0.7
-      charset = (0x00 ... 0x7f).to_a
-      charset += (0x80 ... 0x7ff).to_a
+      charset = (0x00 ... 0x80).to_a
+      charset += (0x80 ... 0x800).to_a
     elsif seed < 0.9
-      charset = (0x00 ... 0x7f).to_a
-      charset += (0x80 ... 0x7ff).to_a
-      charset += (0x800 ... 0x7fff).to_a
+      charset = (0x00 ... 0x80).to_a
+      charset += (0x80 ... 0x800).to_a
+      charset += (0x800 ... 0x8000).to_a
     elsif seed <= 1
-      charset = (0x00 ... 0x7f).to_a
-      charset += (0x80 ... 0x7ff).to_a
-      charset += (0x800 ... 0x7fff).to_a
-      charset += (0x1000 ... 0x1ffff).to_a
+      charset = (0x00 ... 0x80).to_a
+      charset += (0x80 ... 0x800).to_a
+      charset += (0x800 ... 0x8000).to_a
+      charset += (0x1000 ... 0x20000).to_a
     end
 
     ret = []
