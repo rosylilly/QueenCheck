7fb4dde1eb521d47881f599f4f85df77229b09cd
Sho Kusano <rosylilly@aduca.org>
2012-01-22 20:56:17 +0900

    Configureが抜けてた

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,10 @@ require 'spork'
 require 'rspec'
 
 Spork.prefork do
-  config(:before) do
-    @lib_path = Pathname.new(File.expand_path('../../lib/', __FILE__))
+  Rspec.configure do | config |
+    config(:before) do
+      @lib_path = Pathname.new(File.expand_path('../../lib/', __FILE__))
+    end
   end
 end
 
