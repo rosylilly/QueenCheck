11480108c5583e4ed3c7cfb310f0929d6a25e784
Sho Kusano <rosylilly@aduca.org>
2012-02-17 07:44:37 +0900

    String Arbitrary だいたいできた

--- lib/queencheck/arbitraries/string.rb
+++ lib/queencheck/arbitraries/string.rb
@@ -1,4 +1,17 @@
 require 'queencheck/arbitrary'
 
+class Char
+  arbitrary QueenCheck::Gen.choose(0, 127).bind { | c |
+    QueenCheck::Gen.unit(c.chr)
+  }
+end
+
 class String
+  arbitrary QueenCheck::Gen.quadratic(20000).bind { | length |
+    QueenCheck::Gen.rand.resize(0, length).bind { | r |
+      str = []
+      r.times { str << Char.arbitrary.gen.value(0) }
+      QueenCheck::Gen.unit(str.join())
+    }
+  }
 end
